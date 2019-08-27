2e1a514d8915777f35df87f392d7e55b8df3d29d
soominimini <lovesoomindl@naver.com>
2019-08-27 17:14:41 +0900

    ibm api 통신과 tweet 유저 단위로 크롤링, textarea로 유저 아이디 얻어오기

--- app.py
+++ app.py
@@ -21,10 +21,19 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
+#flask 기반 웹 프로그래밍 라이브러리
+from flask import Flask, render_template, request, send_from_directory, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
+
 url = 'https://gateway.watsonplatform.net/personality-insights/api'
 apikey = '9aPTQUgHwV74s4gjD7zC4ZsCppop7j1-gNheGmW8S6UA'
 service = PersonalityInsightsV3(url = url ,iam_apikey= apikey, version= '2017-10-13')
 
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
 
 days_range = []
@@ -48,31 +57,42 @@ end_date = (datetime.datetime.strptime(days_range[-1], "%Y-%m-%d")
             + datetime.timedelta(days=1)).strftime("%Y-%m-%d") # setUntil이 끝을 포함하지 않으므로, day + 1
 
 
-tweetCriteria = got.manager.TweetCriteria().setUsername("Oprah") \
-                                            .setSince(start_date) \
-                                            .setUntil(end_date) \
-                                            .setMaxTweets(-1)
-tweet = got.manager.TweetManager.getTweets(tweetCriteria)
 
-print("Collecting data start.. from {} to {}".format(days_range[0], days_range[-1]))
-start_time = time.time()
+@app.route('/', methods=['GET','POST'])
+def init_page():
+    if request.method=='GET':
+
+        return render_template('index.html')
+
+    elif request.method=='POST':
+        tweet_id = request.form['txt']
+
+        tweetCriteria = got.manager.TweetCriteria().setUsername(tweet_id+"") \
+            .setSince(start_date) \
+            .setUntil(end_date) \
+            .setMaxTweets(-1)
+        tweet = got.manager.TweetManager.getTweets(tweetCriteria)
+
+        print("Collecting data start.. from {} to {}".format(days_range[0], days_range[-1]))
+        start_time = time.time()
+
+        print("Collecting data end.. {0:0.2f} Minutes".format((time.time() - start_time) / 60))
+        print("=== Total num of tweets is {} ===".format(len(tweet)))
+
+        # initialize
+        tweet_list = ""
+
+        for i in range(len(tweet)):
+            tweet_list += tweet[i].text
+
+        # string 결과값 보내기
+        profile = service.profile(tweet_list, content_type='text/plain', content_language='ko',
+                                  consumption_preferences=True).get_result()
+
+        # 결과값 전체 다 가져오기
+        print(json.dumps(profile, indent=5))
 
 
-print("Collecting data end.. {0:0.2f} Minutes".format((time.time() - start_time)/60))
-print("=== Total num of tweets is {} ===".format(len(tweet)))
-
-# initialize
-tweet_list = ""
-
-for i in range(len(tweet)):
-    tweet_list+= tweet[i].text
-#for index in tqdm_notebook(tweet):
- #   print(tweet[index])
-
-
-# string 결과값 보내기
-profile = service.profile(tweet_list, content_type='text/plain', content_language='ko',
-                          consumption_preferences=True).get_result()
-
-# 결과값 전체 다 가져오기
-print(json.dumps(profile, indent=5))
+#시작
+if __name__ == '__main__':
+    app.run(debug=True)
