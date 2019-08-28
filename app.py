2d98fe95c993fdb6300d1bc60e8322e620209e1d
soominimini <lovesoomindl@naver.com>
2019-08-28 17:52:25 +0900

    ibm api 통신과 tweet 유저 단위로 크롤링, textarea로 유저 아이디 얻어오기, DB저장

--- app.py
+++ app.py
@@ -2,25 +2,18 @@ import GetOldTweets3 as got
 import datetime, time
 from random import uniform
 
-from django.db.models.expressions import Col
-from tqdm import tqdm_notebook
-
 #아래는 ibm API
 # -*- coding: utf-8 -*-
 
 #import personaliy insights
-from builtins import print
+from builtins import print, range
 
 from watson_developer_cloud import PersonalityInsightsV3
+from time import sleep
 #from ibm_watson import PersonalityInsightsV3
 import json
 from openpyxl import load_workbook
 import numpy as np
-
-#아래는 시각화를 위한 라이브러리
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 
 #flask 기반 웹 프로그래밍 라이브러리
@@ -49,9 +42,29 @@ class User(db.Model):
     def __init__(self, id =None):
         id = self.id
 
+class Card(db.Model):
+    id  = Column(db.Integer, primary_key=True) #상점 코드로 id를 사용할 것임
+    store_type = Column(db.VARCHAR(30))
 
+    def __init__(self, id = None):
+        id = self.id
 
+class User_Card(db.Model):
+    id = Column(db.Integer, primary_key=True)
+    code_store = Column(db.VARCHAR(200))
 
+    def __init__(self, id = None):
+        id = self.id
+        
+class Data_Ret(db.Model): #왓슨의 소비성향 카테고리는(소분류) 42개
+    id = Column(db.Integer, primary_key=True) #유저의 id
+    automobile_ownership_cost = Column(db.Float)
+    automobile_safety =  Column(db.Float)
+    clothes_quality =  Column(db.Float)
+
+    def __init__(self, id = None):
+        id = self.id
+        
 days_range = []
 
 start = datetime.datetime.strptime("2019-01-01", "%Y-%m-%d")
@@ -82,6 +95,18 @@ def init_page():
 
     elif request.method=='POST':
         tweet_id = request.form['txtsns']
+        platfortm_id = request.form['txtid']
+        password = request.form['txtpassword']
+        #프론트 엔드에서 보내는 값을 받음
+
+        #DB에 insert
+        user  = User()
+        user.platform_id = platfortm_id
+        user.sns_id = tweet_id
+        user.password = password
+        db.session.add(user)
+        db.session.commit()
+
 
         tweetCriteria = got.manager.TweetCriteria().setUsername(tweet_id+"") \
             .setSince(start_date) \
@@ -101,12 +126,35 @@ def init_page():
         for i in range(len(tweet)):
             tweet_list += tweet[i].text
 
+        sleep(5)
         # string 결과값 보내기
         profile = service.profile(tweet_list, content_type='text/plain', content_language='ko',
                                   consumption_preferences=True).get_result()
 
         # 결과값 전체 다 가져오기
         print(json.dumps(profile, indent=5))
+        i=1
+        ret = Data_Ret()
+        tmp_id =User.query.count()
+        ret_category1 = {personality['consumption_preference_id']: personality['score'] for personality in  profile['consumption_preferences'][0]['consumption_preferences']}
+
+        for personality in profile['consumption_preferences'][0]['consumption_preferences']:
+            print(i)
+            print(personality['score'])
+
+
+        ret_category2 = {personality['consumption_preference_id']: personality['score'] for personality in profile['consumption_preferences'][1]['consumption_preferences']}
+        ret_category3 = {personality['consumption_preference_id']: personality['score'] for personality in  profile['consumption_preferences'][2]['consumption_preferences']}
+        ret_category4 = {personality['consumption_preference_id']: personality['score'] for personality in profile['consumption_preferences'][3]['consumption_preferences']}
+        ret_category5 = {personality['consumption_preference_id']: personality['score'] for personality in profile['consumption_preferences'][4]['consumption_preferences']}
+        ret_category6 = {personality['consumption_preference_id']: personality['score'] for personality in profile['consumption_preferences'][5]['consumption_preferences']}
+        ret_category7 = {personality['consumption_preference_id']: personality['score'] for personality in  profile['consumption_preferences'][6]['consumption_preferences']}
+        ret_category8 = {personality['consumption_preference_id']: personality['score'] for personality in  profile['consumption_preferences'][7]['consumption_preferences']}
+        #총 8개의 대분류
+     #   print(ret_category1)
+      #  print(ret_category2)
+       # print(ret_category3)
+
 
 
 #시작
